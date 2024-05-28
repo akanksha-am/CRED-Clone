9092800437c3e7df09437e2ed8b6891c5509f5a4
Subham Sourav Brahma <subham.sourav.brahma@sap.com>
2024-05-28 14:48:38 +0530

    Solved /profile error

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import BrandCarousel from "../components/BrandCarousal";
 import Branding from "../components/Branding";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserDetails } from "../redux/actions/userActions";
 
 const HomeScreen = () => {
+  // const dispatch = useDispatch();
+  // const { userInfo } = useSelector((state) => state.user);
+  // useEffect(() => {
+  //   if (userInfo !== null) {
+  //     dispatch(getUserDetails());
+  //   }
+  // });
   return (
     <>
       <Branding />
